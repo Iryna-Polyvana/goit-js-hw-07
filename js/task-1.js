647d8641510b3709cd0a83f4c394113896e62b1a
Iryna Polyvana <irynapolyvana@gmail.com>
2025-11-06 09:20:51 +0100

    task 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,4 @@
-const categories = document.getElementById('categories');
+const categories = document.querySelector('#categories');
 const items = categories.querySelectorAll('.item');
 console.log(`Number of categories: ${items.length}`);
 items.forEach(categories => {
