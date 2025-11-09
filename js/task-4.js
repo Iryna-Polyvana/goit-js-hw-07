dc51101895fec2a6c52ea1625798f9bff0cd004c
Iryna Polyvana <irynapolyvana@gmail.com>
2025-11-09 16:58:09 +0100

    task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const refs = {
+    loginForm: document.querySelector('.login-form'),
+}
+const loginFormSubmit = event => {
+    event.preventDefault();
+
+    const loginFormData = {
+        email: refs.loginForm.elements.email.value.trim(),
+        password: refs.loginForm.elements.password.value.trim(),
+    };
+
+    const loginFormDataValue = Object.values(loginFormData);
+    if (loginFormDataValue.includes('')) {
+        alert('All form fields must be filled in')
+        return;
+    }
+
+    console.log(loginFormData);
+    refs.loginForm.reset();
+};
+refs.loginForm.addEventListener('submit', loginFormSubmit)
 
