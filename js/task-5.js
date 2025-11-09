4cfc018cd7cf8064a64967b9732c999d1760a64f
Iryna Polyvana <irynapolyvana@gmail.com>
2025-11-09 19:30:16 +0100

    task 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,18 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  changeColorBtn: document.querySelector('.change-color'),
+  spanColor: document.querySelector('.color'),
+  body: document.querySelector('.widget')
+};
+
+refs.changeColorBtn.addEventListener('click', event => {
+  refs.spanColor.style.backgroundColor = getRandomHexColor();
+  refs.body.style.backgroundColor = refs.spanColor.style.backgroundColor;
+});
+// На що буде звертати увагу ментор при перевірці:
+// Фон на < body > задається тільки після кліку на button.change - color
+// При кожному кліку на елемент button.change - color фон < body > зафарбовується новим рандомним кольором
+// На < body > і span.color значення одного й того самого кольору
